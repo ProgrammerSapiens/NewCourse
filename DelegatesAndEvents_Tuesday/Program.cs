89829b79279bd1bb4709f31d83534f226a1eb1ef
Dmitriy <homyakov-dmitriy@mail.ru>
2024-11-19 18:08:57 +0200

    Создание и использование делегата для выполнения арифметических операций

---
+++ DelegatesAndEvents_Tuesday/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DelegatesAndEvents_Tuesday
+{
+    internal class Program
+    {
+        public delegate int Operation(int x, int y);
+        static void Main(string[] args)
+        {
+            Operation operation = Add;
+            int x = 10;
+            int y = 12;
+            Console.WriteLine($"Addition = {operation(x, y)}");
+
+            operation = Subtract;
+            Console.WriteLine($"Subtraction = {operation(x, y)}");
+
+            operation = Multiply;
+            Console.WriteLine($"Multiplication = {operation(x, y)}");
+
+            operation = Divide;
+            Console.WriteLine($"Division = {operation(x, y)}");
+        }
+        static public int Add(int x, int y) => x + y;
+        static public int Subtract(int x, int y) => x - y;
+        static public int Multiply(int x, int y) => x * y;
+        static public int Divide(int x, int y) => x / y;
+
+    }
+}
