ad0cf455572823ca2a275074959067dd17f2de8f
Dmitriy <homyakov-dmitriy@mail.ru>
2024-11-20 20:14:06 +0200

    Разработка упрощенной системы управления событиями для онлайн-магазина

---
+++ DelegatesAndEvents_Wednesday/Program_Two.cs
@@ -0,0 +1,25 @@
+﻿using DelegatesAndEvents_Wednesday.Second;
+
+namespace DelegatesAndEvents_Wednesday
+{
+    internal class Program_Two
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager();
+            NotificationService notificationService = new NotificationService();
+
+            productManager.ProductAdded += notificationService.HandleProductAdded;
+            productManager.ProductUpdated += notificationService.HandleProductUpdated;
+            productManager.ProductRemoved += notificationService.HandleProductRemoved;
+
+            var product1 = new Product { Name = "Товар 1", Price = 100 };
+            var product2 = new Product { Name = "Товар 2", Price = 200 };
+
+            productManager.AddProduct(product1);
+            productManager.AddProduct(product2);
+            productManager.UpdateProduct("Товар 1", new Product { Name = "Обновленный Товар 1", Price = 120 });
+            productManager.RemoveProduct("Товар 2");
+        }
+    }
+}
---
+++ DelegatesAndEvents_Wednesday/Second/ProductManager.cs
@@ -0,0 +1,85 @@
+﻿namespace DelegatesAndEvents_Wednesday.Second
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class ProductEventArgs : EventArgs
+    {
+        public Product Product { get; }
+        public ProductEventArgs(Product product)
+        {
+            Product = product;
+        }
+    }
+
+    internal class ProductManager
+    {
+        private List<Product> products = new List<Product>();
+        public event EventHandler<ProductEventArgs> ProductAdded;
+        public event EventHandler<ProductEventArgs> ProductUpdated;
+        public event EventHandler<ProductEventArgs> ProductRemoved;
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+            OnProductAdded(new ProductEventArgs(product));
+        }
+
+        public void UpdateProduct(string productName, Product updatedProduct)
+        {
+            var product = products.Find(p => p.Name == productName);
+            if (product != null)
+            {
+                product.Name = updatedProduct.Name;
+                product.Price = updatedProduct.Price;
+                OnProductUpdated(new ProductEventArgs(product));
+            }
+        }
+
+        public void RemoveProduct(string productName)
+        {
+            var product = products.Find(p => p.Name == productName);
+            if (product != null)
+            {
+                products.Remove(product);
+                OnProductRemoved(new ProductEventArgs(product));
+            }
+        }
+
+        protected virtual void OnProductAdded(ProductEventArgs e)
+        {
+            ProductAdded?.Invoke(this, e);
+        }
+
+        protected virtual void OnProductUpdated(ProductEventArgs e)
+        {
+            ProductUpdated?.Invoke(this, e);
+        }
+
+        protected virtual void OnProductRemoved(ProductEventArgs e)
+        {
+            ProductRemoved?.Invoke(this, e);
+        }
+    }
+
+    public class NotificationService
+    {
+        public void HandleProductAdded(object sender, ProductEventArgs e)
+        {
+            Console.WriteLine($"Новый продукт добавлен: {e.Product.Name} за {e.Product.Price}.");
+        }
+
+        public void HandleProductUpdated(object sender, ProductEventArgs e)
+        {
+            Console.WriteLine($"Продукт обновлен: {e.Product.Name}.");
+        }
+
+        public void HandleProductRemoved(object sender, ProductEventArgs e)
+        {
+            Console.WriteLine($"Продукт удален: {e.Product.Name}.");
+        }
+    }
+}
