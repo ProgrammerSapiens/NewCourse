44608fc208a57c9a01f9d8c09c3a281c14de6c06
Dmitriy <homyakov-dmitriy@mail.ru>
2024-12-09 13:45:09 +0200

    Работа с API в ASP.NET Core

---
+++ WebDevelopmentAndAPI.SecondDay/Controllers/UserController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDevelopmentAndAPI.SecondDay.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _userService.LoadUsers();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetUsersById(int id)
+        {
+            var users = _userService.LoadUsers();
+            var user = users.FirstOrDefault(x => x.Id == id);
+
+            if (user != null)
+            {
+                return Ok(user);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult CreateUser([FromBody] User newUser)
+        {
+            _userService.AddUser(newUser);
+            return CreatedAtAction(nameof(GetUsers), new { id = newUser.Id }, newUser);
+        }
+    }
+}
---
+++ WebDevelopmentAndAPI.SecondDay/UserService.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+
+namespace WebDevelopmentAndAPI.SecondDay
+{
+    public class UserService
+    {
+        private readonly string filePath;
+
+        public UserService(IWebHostEnvironment env)
+        {
+            filePath = Path.Combine(env.ContentRootPath, "users.json");
+        }
+
+        public List<User> LoadUsers()
+        {
+            if (!File.Exists(filePath))
+                return new List<User>();
+
+            string json = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<User>>(json) ?? new List<User>();
+        }
+
+        public void AddUser(User user)
+        {
+            var users = LoadUsers();
+            users.Add(user);
+
+            string json = JsonSerializer.Serialize(users, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filePath, json);
+        }
+    }
+}
