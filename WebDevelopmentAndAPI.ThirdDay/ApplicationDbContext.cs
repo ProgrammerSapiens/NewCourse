4a8fdfc3d70b40a134656a0c59eefab5976d3d3c
Dmitriy <homyakov-dmitriy@mail.ru>
2024-12-10 15:16:39 +0200

    Изучение темы Подключение к базе данных

---
+++ WebDevelopmentAndAPI.ThirdDay/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebDevelopmentAndAPI.ThirdDay
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
---
+++ WebDevelopmentAndAPI.ThirdDay/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebDevelopmentAndAPI.ThirdDay.Controllers
+{
+    [Route("api/[controller]")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateUser(string name)
+        {
+            var user = new User { Name = name };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<User>> GetUsersFromDb()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
